04c1b84b5c393b3dbf25d736335d203328e7af96
Sergey Milleu <33151983+prasx@users.noreply.github.com>
2023-03-09 15:09:09 +0300

    Update v0.4
    v0.4

--- bot.py
+++ bot.py
@@ -2,16 +2,23 @@ import openai
 import telebot
 import time
 import logging
+import os
 
 # Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w")
-# API доступы
-openai.api_key = "< YOU API KEY >"
-bot = telebot.TeleBot("< YOU API KEY >")
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
+logging.debug("A DEBUG Message")
+logging.info("An INFO")
+logging.warning("A WARNING")
+logging.error("An ERROR")
+logging.critical("A message of CRITICAL severity")
 
-users = {}
+# API доступы
+openai.api_key = " API "
+bot = telebot.TeleBot(" API ")
 stop_symbols = "###"
-model = "text-davinci-003"
+model = "text-davinci-003" # выбор языковой модели, лучшая на текущий момент text-davinci-003 //  gpt-3.5-turbo // gpt-3.5-turbo-0301 - до июня 2023
+users = {}
+
 
 
 def _get_user(id):
@@ -33,21 +40,21 @@ def _process_rq(user_id, rq):
 
        # Удаляем все что полсе 2000 символов
         prompt = f" {last_text} {rq} -> "[-2000:]
-        print("Sending:" + prompt) 
+        print("Sending:" + prompt)
         completion = openai.Completion.create(
-            engine=model, prompt=prompt, max_tokens=512, stop=[stop_symbols], temperature=0.8)
+            engine=model, prompt=prompt, max_tokens=1024, stop=[stop_symbols], temperature=0.8)
         eng_ans = completion['choices'][0]['text'].strip()
         if "->" in eng_ans:
             eng_ans = eng_ans.split("->")[0].strip()
         ans = eng_ans
-        print(f"<<< ({user_id}) {ans}") 
+        print(f"<<< ({user_id}) {ans}")
         user['last_text'] = prompt + " " + eng_ans + stop_symbols
         user['last_prompt_time'] = time.time()
         return ans
     else:
         user['last_prompt_time'] = 0
         user['last_text'] = ''
-        return "Ошибка."
+        return "Ошибка!"
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -56,8 +63,8 @@ def send_welcome(message):
     user['last_prompt_time'] = 0
     user['last_text'] = ''
     bot.reply_to(message, f"\nПривет,  {message.from_user.first_name}!"
-                          f"\nИстория сообщения: очищена 🦧"
-                          f"\nВыбранная языковая модель: {model}")
+                 f"\nИстория сообщения: очищена 🦧"
+                 f"\nВыбранная языковая модель: {model}")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
@@ -66,7 +73,11 @@ def echo_all(message):
     ans = _process_rq(user_id, rq)
     bot.send_message(message.chat.id, ans)
 
-
-
 if __name__ == '__main__':
-    bot.polling()
+    while True:
+        try:
+            bot.polling(True)
+        except Exception:
+            logging.exception('Возникла ошибка.')
+            time.sleep(10)
+            os.execl(sys.executable, sys.executable, *sys.argv)
